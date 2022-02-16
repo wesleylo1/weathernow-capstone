a7ad323549a1b7b4d9ab6c45b06f50f68329b467
Wesley Lo <wesleylo1995@gmail.com>
2022-02-16 17:20:45 -0600

    update

--- server/server.js
+++ server/server.js
@@ -37,5 +37,5 @@ app.get('/api/weather', getWeather)
 
 
 
-const port = SERVER_PORT
+const port = process.env.PORT || SERVER_PORT
 app.listen(port,() => console.log(`Server running on port ${port}`))
