0a0a595d605f276509b1644e7fe85cea9c02b6ff
Wesley Lo <wesleylo1995@gmail.com>
2022-02-16 20:02:19 -0600

    update server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '../public/')))
 
 
 app.get('/', (req,res) => {
